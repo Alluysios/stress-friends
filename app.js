563c4d300fddd1db890e9425b912aaf48e3bd9d9
Alluysios <arribaallu@gmail.com>
2020-08-30 12:36:27 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
+/*
+===================
+ GLOBAL MIDDLEWARE 
+===================
+*/
+// Compress Files
+app.use(compression());
+// Security HTTP Headers
+app.use(helmet());
+
+// Development logging
+if(process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (prevent HTML Codes)
+app.use(xss());
+
+// Body parser, reading data from the body into the req.body (limit 10kb)
+app.use(express.json({ limit: '10kb' }));
+
+// parse data from urlencoded form (files), {extended: true} = pass complex data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Global error handling this means the response cycle didn't make it if we reach this point.
+app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Set environment file path
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const db = process.env.DATABASE_SF;
+// Connect to Database
+mongoose.connect(db, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }    
+).then(() => {
+    console.log('Database connected');
+}).catch(() => {
+    console.log('Something is wrong with the database');
+})
+
+// PORT *required for HEROKU DEPLOYMENT
+const PORT = process.env.PORT || 8000;
+
+// Server listen/start
+app.listen(PORT, () => {
+    console.log(`App listening on port:${PORT}`);
+});
