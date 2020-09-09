f8123d9c112b408581efec888967a9694a5bdb33
Alluysios <arribaallu@gmail.com>
2020-09-09 16:47:45 -0600

    static files

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ app.use('/api/v1/users', userRouter);
 
 // Serve static files
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
