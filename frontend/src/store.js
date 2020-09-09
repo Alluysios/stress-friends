34973c73c36274e5fa4386346e752a13d0c6487f
Alluysios <arribaallu@gmail.com>
2020-09-09 15:28:41 -0600

    removed dependancy

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,10 @@
 
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 
 const middleware = [thunk];
 
-const store = createStore(reducer, {}, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(reducer, {}, applyMiddleware(...middleware));
 
 export default store;
