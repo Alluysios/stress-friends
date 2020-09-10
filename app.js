b792795fac98cda76b05679b975c42adf80e83cc
Alluysios <arribaallu@gmail.com>
2020-09-09 21:47:57 -0600

    initial commit

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ app.use('/api/v1/users', userRouter);
 
 if(process.env.NODE_ENV === 'production') {
     // Serve static files
-    app.use(express.static("frontend/build"));
+    app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
